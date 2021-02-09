8146e672bd462905c96fc9c7f46cd068f7b51b2b
rajaschavan19 <63785438+rajaschavan19@users.noreply.github.com>
2021-02-09 23:01:39 +0100

    first

---
+++ learning/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+
+from basic_app import views
+
+
+app_name= 'basic_app'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+]
